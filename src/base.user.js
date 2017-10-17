1563a96b88afde9673a94f46591e9a0b2170e870
Yotam Salmon <yotam.salmon@gmail.com>
2017-10-17 22:01:46 +0300

    Add SkyX.Debugger

--- src/base.user.js
+++ src/base.user.js
@@ -35,6 +35,55 @@ SkyX.LocalStorageException = function(message) {
 	this.type = "LocalStorageException";
 };
 
+SkyX.Debugger = function() {
+	
+	/*
+	Description:
+		Will output a console warning and optionally state what extension it came from.
+	
+	Parameters:
+		string - message - the message of the warning.
+		object -  module - the extension object (usually just passing `this` would be enough)
+		
+	Return value:
+		none
+	*/
+	this.warn = function(message, module) {
+		if (module) {
+			console.warn("[" + module.name + "] SkyX Debugger: " + message);
+		}
+		else {
+			console.warn("SkyX Debugger: " + message);
+		}
+	};
+	
+	/*
+	Description:
+		Sets global window tag functions like __DEPRECATED__ or __UNTESTED__ or __NOT_WORKING__
+		Put these tags only inside functions - in the first line so they are clearly visible.
+	*/
+	this.set_window_tags = function() {
+		let warn = this.warn;
+		
+		window.__DEPRECATED__ = function() {
+			warn("__DEPRECATED__ alert for `" + arguments.callee.caller.name + "`");
+		};
+		window.__UNTESTED__ = function() {
+			warn("__UNTESTED__ alert for `" + arguments.callee.caller.name + "`");
+		};
+		window.__NOT_WORKING__ = function() {
+			warn("__NOT_WORKING__ alert for `" + arguments.callee.caller.name + "`");
+		};
+		window.__BAD__ = function() {
+			warn("__BAD__ alert for `" + arguments.callee.caller.name + "`");
+		};
+		
+	};
+	
+	this.set_window_tags();
+	
+};
+
 /*
 Description:
 	The main class of base.user.js - the SkyXBase is responsible for the tasks listed in the top of the file.
@@ -82,10 +131,62 @@ SkyX.SkyXBase = function() {
 		return localStorage.getItem("skyx_core") || null;
 	};
 	
+	/*
+	Description:
+		returns the global `geofs` object.
+		Please do not use the global `geofs`. Call this method or use a parameter-given `geofs` (in order not to break CI 'builds' if there will be any)
+		
+	Return values:
+		object - the `geofs` object.
+		null - if the global `geofs` object is undefined or null.
+	*/
+	this.query_geofs = function() {
+		return window["geofs"] || null;
+	};
+	
+	/*
+	Description:
+		Waits asynchronously for the `geofs` object to load, then calls a callback.
+	
+	Parameters:
+		invocable - callback - the callback to be invoked once GeoFS is loaded.
+			Invokes the callback with the geofs parameter.
+	
+	Return values:
+		none
+	*/
+	this.wait_geofs = function(callback) {
+		__UNTESTED__();
+		
+		let query_geofs = this.query_geofs;
+
+		let a = function() {
+			setTimeout(function() {
+				if (query_geofs()) {
+					callback(query_geofs());
+				}
+				else {
+					// This is **not** recursive. Just invokes itself in a timeout (No traces are left on the stack). Should not create any problems.
+					a(callback); 
+				}
+			}, 2);
+		};
+		a();
+	};
+	
 };
 
+/*
+Description:
+	Main code of base.user.js
+*/
 (function() {
 	
+	window.SkyXBase = new SkyX.SkyXBase();
+	window.SkyXDebug = new SkyX.Debugger();
 	
+	SkyXBase.wait_geofs(function(fs) {
+		// Do things after GeoFS is loaded.
+	});
 	
 })();
