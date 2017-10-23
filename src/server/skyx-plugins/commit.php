708664f9910b5d2e2118e77a904a4defeb54294a
Yotam Salmon <yotam.salmon@gmail.com>
2017-10-23 22:02:16 +0300

    Start webhook #2

--- src/server/skyx-plugins/commit.php
+++ src/server/skyx-plugins/commit.php
@@ -1,1 +1,8 @@
-<?php echo "ok"; ?>
+<?php
+
+$payload = json_decode($_POST["payload"]);
+$sha1 = $_SERVER["HTTP_X_HUB_SIGNATURE"];
+$event = $_SERVER["HTTP_X_GITHUB_EVENT"];
+echo $sha1 . ", " . $event;
+
+?>
--- src/server/skyx-plugins/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?
-session_start();
-$_SESSION["login"] = "";
-session_destroy();
-session_write_close();
-header("Location: /");
-?>
