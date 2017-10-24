adab79af82603d3ed2271e5cd883ead4eae855dd
Yotam Salmon <yotam.salmon@gmail.com>
2017-10-24 12:54:30 +0300

    Start base.user.js fixes

--- src/client/base.user.js
+++ src/client/base.user.js
@@ -4,7 +4,8 @@
 // 1. In the first run of the plugin, download the main.user.js from the server, store it into the localStorage
 // 2. Wait for jQuery and GeoFS objects to load in the page, then load the main.user.js from the localStorage and run it
 
-// TODO - make the file
+// TODO - Fix mistakes and change the method Base auto-updates
+
 
 /*
 Description:
