4f64a1c011c7c7b2181479bfab9a5a04097bbfbe
ron-popov <ron.popov@hotmail.com>
2017-10-24 19:03:09 +0300

    Finished working core.user.js, haven't tested yet

--- src/client/core.user.js
+++ src/client/core.user.js
@@ -25,27 +25,87 @@ function main() {
 	//if this browser suppoprt Local Storage
 	if(typeof(Storage) !== "undefined") {
 
-		// load ui.user.html into a settings collapsable item
-		var baseContentUrl = "https://raw.githubusercontent.com/geofs-plugins/plugin-manager-V2/master/";
+		// loding the ui
+		let remoteContentUrl = "https://raw.githubusercontent.com/geofs-plugins/plugin-manager-V2/master/";
+		var uiData = localStorage.getItem("ui.user.html");
+		if (uiData === null) {
+			// ALERT : please wait for the ui file to download
+		} else {
+			// load the ui
+			$(".geofs-list").append($("li")
+					.addClass("geofs-list-collapsible-item")
+					.html(uiData));
+		}
 
+		// updating files
 		$.ajax({
-			url : baseContentUrl + "src/client/ui.base.html",
+			url : "https://api.github.com/repos/geofs-plugins/plugin-manager-V2/commits/release",
 			callback : function(data) {
-				$(".geofs-list").append($("li").addClass("geofs-list-collapsible-item").html(data));
+				let latestRemoteCommitHash = data["sha"];
+
+				if(latestRemoteCommitHash != localStorage.getItem("latestRemoteCommitHash")) {
+
+					let filesToUpdate = ["core.user.js", "ui.user.html"];
+
+					// if one of the updates failed then delete everything
+					// and hope for the best
+					let hasFailed = false;
+					let filesFinished = 0;
+
+					// wait for the files to finish update and notify
+					// the user accordingly
+					function waitForUpdate() {
+						if (filesFinished = filesToUpdate.length) {
+							if (hasFailed) {
+								// ALERT : An error occured
+
+								// deleting all of the files
+								for (var file in filesToUpdate) {
+									localStorage.setItem(file, null);
+								}
+							} else {
+								localStorage.setItem("latestRemoteCommitHash", latestRemoteCommitHash);
+								// ALERT : everything has finished, please refresh
+							}
+						} else {
+							setTimeout(function() {waitForUpdate()} , 500);
+						}
+					}
+
+					waitForUpdate();
+
+					// go over all of the files and update them all
+					for (let file in filesToUpdate) {
+						$.ajax({
+							url : remoteContentUrl + "src/client/" + file,
+
+							callback : function(data) {
+								if((!("ui.user.html" in localStorage)) && file == "ui.user.html") {
+									$(".geofs-list").append($("li")
+											.addClass("geofs-list-collapsible-item")
+											.html(uiData));
+								}
+
+								localStorage.setItem(file, data);
+								filesFinished++;
+							} ,
+
+							error : function(){
+								hasFailed = false;
+								filesFinished++;
+							}
+						});
+					}
+				}
 			}
 		});
+
 
 		var CORE_VERSION = "0.1.0";
 		SkyXDebug.log("Core has been loaded");
 	} else {
-		console.log("Sorry, SkyX cannot run without local storage support");
+		console.log("Sorry, SkyX cannot run at the moment without local storage support");
 	}
-
-
-
-	// TODO : Update core.user.js
-	// TODO : Update ui.user.html
-
 });
 
 // TODO : Adding some lines to trigger WebHook
