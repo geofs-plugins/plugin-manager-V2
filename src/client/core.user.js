d18728f8e17224ca539e7ec8db30564377a68e74
ron-popov <DirtySlowpoke@gmail.com>
2017-10-29 22:01:25 +0200

    Tidied up core.user.js and proper division into functions

--- src/client/core.user.js
+++ src/client/core.user.js
@@ -20,7 +20,6 @@ function waitForJquery(method) {
 	}
 }
 
-
 // tells the user a message
 function notify(msg) {
 	// TODO : Implement
@@ -32,14 +31,11 @@ function debug(msg) {
 	console.log(msg);
 }
 
-
-// main function
-function main() {
-
-	//if this browser suppoprt Local Storage
-	if (typeof(Storage) !== "undefined") {
-
-		// loding the ui
+// TODO : Fix this, see github issue
+// loads the ui from local storage
+// and inserts it into the page
+function loadUi() {
+		// loading the ui
 		let remoteContentUrl = "https://raw.githubusercontent.com/geofs-plugins/plugin-manager-V2/release/";
 		var uiData = localStorage.getItem("SkyX/ui.user.html");
 		if (uiData === null) {
@@ -50,88 +46,118 @@ function main() {
 				.addClass("geofs-list-collapsible-item")
 				.html(uiData));
 		}
+}
 
-		// updating files
-		$.ajax({
-			url: "https://api.github.com/repos/geofs-plugins/plugin-manager-V2/commits/release",
-			success: function(data) {
-				debug("Succesfuly got latest commit hash");
-				let latestRemoteCommitHash = data["sha"];
+// TODO : Implement this
+// loads the plugins from memory
+function loadPlugins() {
 
-				if (latestRemoteCommitHash != localStorage.getItem("SkyX/version")) {
+}
 
-					let filesToUpdate = ["core.user.js", "ui.user.html"];
+// TODO : Implement this
+// updates the plugins
+function updatePlugin() {
 
-					// if one of the updates failed then delete everything
-					// and hope for the best
-					let hasFailed = false;
-					let filesFinished = 0;
+}
+
+// TODO : Handle download failure better, see github issue
+// updates core.user.js and saves it into localStorage
+function updateSelf() {
+
+	// updating files
+	$.ajax({
+		url: "https://api.github.com/repos/geofs-plugins/plugin-manager-V2/commits/release",
+		success: function(data) {
+			debug("Succesfuly got latest commit hash");
+			let latestRemoteCommitHash = data["sha"];
+
+			if (latestRemoteCommitHash != localStorage.getItem("SkyX/version")) {
+
+				let filesToUpdate = ["core.user.js", "ui.user.html"];
+
+				// if one of the updates failed then delete everything
+				// and hope for the best
+				let hasFailed = false;
+				let filesFinished = 0;
 
 
-					// wait for the files to finish update and notify
-					// the user accordingly
-					function waitForUpdate() {
-						console.log("Testing stuff out");
-						if (filesFinished == filesToUpdate.length) {
-							if (hasFailed) {
-								notify("One of the files failed to download");
+				// wait for the files to finish update and notify
+				// the user accordingly
+				function waitForUpdate() {
+					console.log("Testing stuff out");
+					if (filesFinished == filesToUpdate.length) {
+						if (hasFailed) {
+							notify("One of the files failed to download");
 
-								// deleting all of the files
-								for (var file in filesToUpdate) {
-									localStorage.setItem("SkyX/" + filesToUpdate[file], null);
-								}
-							} else {
-								localStorage.setItem("SkyX/version", latestRemoteCommitHash);
-								notify("Succesfuly updated SkyX V2, please refresh GeoFS for the changes to take affect");
+							// deleting all of the files
+							for (var file in filesToUpdate) {
+								localStorage.setItem("SkyX/" + filesToUpdate[file], null);
 							}
 						} else {
-							setTimeout(function() {
-								waitForUpdate();
-							}, 50);
+							localStorage.setItem("SkyX/version", latestRemoteCommitHash);
+							notify("Succesfuly updated SkyX V2, please refresh GeoFS for the changes to take affect");
 						}
+					} else {
+						setTimeout(function() {
+							waitForUpdate();
+						}, 50);
 					}
-
-					waitForUpdate();
-
-					// go over all of the files and update them all
-					for (let file in filesToUpdate) {
-						$.ajax({
-							url: remoteContentUrl + "src/client/" + filesToUpdate[file],
-							success: function(data) {
-								debug("got " + filesToUpdate[file]);
-								if ((!("SkyX/ui.user.html" in localStorage)) && filesToUpdate[file] == "ui.user.html") {
-									$(".geofs-list").append($("li")
-										.addClass("geofs-list-collapsible-item")
-										.html(uiData));
-								}
-
-								localStorage.setItem("SkyX/" + filesToUpdate[file], data);
-								filesFinished++;
-							},
-
-							error: function() {
-								debug("failed to download" + filesToUpdate[file]);
-								hasFailed = true;
-								filesFinished++;
-							}
-						});
-					}
-				} else {
-					debug("Everything is up to date");
 				}
-			},
-			error: function() {
-				debug("Getting latest commit hash failed");
+
+				waitForUpdate();
+
+				// go over all of the files and update them all
+				for (let file in filesToUpdate) {
+					$.ajax({
+						url: remoteContentUrl + "src/client/" + filesToUpdate[file],
+						success: function(data) {
+							debug("got " + filesToUpdate[file]);
+							if ((!("SkyX/ui.user.html" in localStorage)) && filesToUpdate[file] == "ui.user.html") {
+								$(".geofs-list").append($("li")
+									.addClass("geofs-list-collapsible-item")
+									.html(uiData));
+							}
+
+							localStorage.setItem("SkyX/" + filesToUpdate[file], data);
+							filesFinished++;
+						},
+
+						error: function() {
+							debug("failed to download" + filesToUpdate[file]);
+							hasFailed = true;
+							filesFinished++;
+						}
+					});
+				}
+			} else {
+				debug("Everything is up to date");
 			}
-		});
+		},
+		error: function() {
+			debug("Getting latest commit hash failed");
+		}
+	});
+}
+
+// main function
+// incharge of updating and loading the appropriate files
+function main() {
+
+	//if this browser suppoprt Local Storage
+	if (typeof(Storage) !== "undefined") {
+
+		loadUi();
+
+		loadPlugins();
+
+		updatePlugins();
+
+		updateSelf();
+
 	} else {
 		notify("ERROR , your browser doesn't support Local Storage");
 	}
 }
 
-
-
-(function() {
-	'use strict';
-	waitForJquery(main);
-})();
+// waits for jQuery to load and then calls
+waitForJquery(main);
