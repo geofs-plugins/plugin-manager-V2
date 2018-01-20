572603947518232eb8d05bebc456a89f63852b17
ron-popov <ron.popov@hotmail.com>
2018-01-20 16:26:42 +0200

    Pretty much everything, no debug tho

--- src/core.user.js
+++ src/core.user.js
@@ -8,6 +8,8 @@
 // 4. Add a UI to manage extensions (add, remove, update, etc.) - WIP
 // 5. Update itself - CHECK
 
+// --------- Variables ---------
+
 // Affects what branch to pull from, dev or release.
 var isDebug = true;
 
@@ -17,10 +19,18 @@ let remoteBranch = (isDebug ? "dev" : "release");
 // A link to the directory that contains all of the remote content
 let remoteContentUrl = "https://rawgit.com/geofs-plugins/plugin-manager-V2/" + remoteBranch + "/src/";
 
+// A temporary queue of plugins that need to be downlaoded
+let downloadQueue = [];
+
+// A list of loaded plugins
+let loadedPlugins = [];
+
+// --------- Utilities ---------
 
 // waits for jQuery to load and then
 // calls the callback function, passed as a parameter
-function waitForJquery(method) {
+function waitForJquery(method) 
+{
 	console.log("waiting for jQuery");
 	if (window.jQuery) {
 		console.log("jQuery found");
@@ -39,19 +49,75 @@ function waitForJquery(method) {
 }
 
 // tells the user a message
-function notify(msg) {
+function notify(msg) 
+{
 	// TODO : Implement
 	alert("Notifications : " + msg);
 }
 
 // debug messages
-function debug(msg) {
+function debug(msg) 
+{
 	console.log(msg);
+}
+
+// --------- SkyX V2 Code ---------
+
+// A recursive method that fetches all of the plugin's dependencies
+// Returns null if some of the dependencies are missing
+var getPluginDependencies = function(pluginId, checkedPlugins)
+{
+	var pluginContent = localStorage.getItem("SkyX/Plugins/" + pluginId + "/main.js");
+	if (pluginContent === undefined)
+	{
+		return null;
+	}
+	else
+	{
+		checkedPlugins.push(pluginId);
+
+		var pluginObject = pluginContent();
+		for(var remoteDependency in pluginObject.remoteDependencies)
+		{
+			var isCheckedAlready = remoteDependency in checkedPlugins;
+			var hasValidDependencies = checkPlugin(checkedPlugins);
+
+			if(!isCheckedAlready && !hasValidDependencies)
+			{
+				return null;
+			}
+
+			if(isCheckedAlready)
+			{
+				// If the plugin already exists in the list than move it to be closer
+				// to the end as the last plugin is the first to be loaded
+				var index = checkedPlugins.indexOf(remoteDependency);
+				isCheckedAlready = checkedPlugins.splice(index, 1);
+				checkedPlugins.push(remoteDependency);
+			}
+			else
+			{
+				checkedPlugins.push(remoteDependency);
+			}
+		}
+	}
+
+	return checkedPlugins;
 }
 
 // loads the ui from local storage
 // and inserts it into the page
-function loadUi() {
+function loadUi() 
+{
+	// Takes the content given and adds that to the game
+	// in the right spot
+	var insertUi = function(content) 
+	{
+		$(".geofs-preference-list").
+			append("<li class='geofs-list-collapsible-item'>SkyX" + 
+				"<ul class='geofs-list'>"+ content +"</ul></li>");
+	}
+
 	// loading the ui
 	var uiData = localStorage.getItem("SkyX/Core/ui.user.html");
 	if (uiData === null) {
@@ -61,29 +127,174 @@ function loadUi() {
 	}
 }
 
-// Takes the content given and adds that to the game
-// in the right spot
-function insertUi(content) {
-	$(".geofs-preference-list").
-		append("<li class='geofs-list-collapsible-item'>SkyX" + 
-			"<ul class='geofs-list'>"+ content +"</ul></li>");
-}
+// Loads the saved plugins and their dependencies
+function loadPlugins() 
+{
+	// A recursive method that fetches all of the plugin's dependencies
+	// Returns null if some of the dependencies are missing
+	var getPluginDependencies = function(pluginId, checkedPlugins)
+	{
+		var pluginContent = localStorage.getItem("SkyX/Plugins/" + pluginId + "/main.js");
+		if (pluginContent === undefined)
+		{
+			return null;
+		}
+		else
+		{
+			checkedPlugins.push(pluginId);
 
-// TODO : Implement this
-// loads the plugins from memory
-function loadPlugins() {
+			var pluginObject = pluginContent();
+			for(var remoteDependency in pluginObject.remoteDependencies)
+			{
+				var isCheckedAlready = remoteDependency in checkedPlugins;
+				var hasValidDependencies = checkPlugin(checkedPlugins);
+
+				if(!isCheckedAlready && !hasValidDependencies)
+				{
+					return null;
+				}
+
+				if(isCheckedAlready)
+				{
+					// If the plugin already exists in the list than move it to be closer
+					// to the end as the last plugin is the first to be loaded
+					var index = checkedPlugins.indexOf(remoteDependency);
+					isCheckedAlready = checkedPlugins.splice(index, 1);
+					checkedPlugins.push(remoteDependency);
+				}
+				else
+				{
+					checkedPlugins.push(remoteDependency);
+				}
+			}
+		}
+
+		return checkedPlugins;
+	}
 	
+	var pluginsTable = localStorage.getItem("SkyX/Plugins/PluginsTable");
+	for (var pluginId in pluginsTable)
+	{
+		// A list of plugins to load for this specified plugin
+		var toLoad = getPluginDependencies(pluginId);
+		if (toLoad !== null)
+		{
+			// A list of plugins to load, dependencies first
+			toLoad = toLoad.reverse();
+
+			for (loadingPluginId in toLoad)
+			{
+				var loadingPluginObject = localStorage.getItem("SkyX/Plugins/" + loadingPluginId + "/main.js")();
+
+				// Eval each one of the plugin's files
+				for (var file in loadingPluginObject.localDependencies)
+				{
+					var fileContent = localStorage.getItem("SkyX/Plugins/" + loadingPluginId + "/" + file);
+					if(fileContent === undefined)
+					{
+						debug("Failed to load " + file);
+					}
+					else
+					{
+						eval(fileContent);
+					}
+				}
+			}
+		}
+		else
+		{
+			debug("Failed to load " + pluginId + " due to missing files");
+		}
+	}
 }
 
-// TODO : Implement this
-// updates the plugins
-function updatePlugin() {
+// Updates/Downloads each on the plugins if necessery
+function updatePlugins() 
+{
+	// Checks for newer version and downloads all of the required dependendencies
+	// Does not handle failures well
+	var updatePlugin = function(pluginId)
+	{
+		let currentCommitHash = localStorage.getItem("SkyX/Plugins/PluginsTable")[pluginId];
 
+		// I know it's not pretty, but that's all we have got and you are gonna have to deal with it
+		// TODO : Add failure handlers
+		$.ajax(
+			{
+				url: "https://api.github.com/repos/geofs-plugins/" + pluginId + "/commits/release",
+				success: function(data)
+				{
+					let remoteCommitHash = data["sha"];
+
+					if(remoteCommitHash !== currentCommitHash)
+					{
+						//Guys , WE HAVE AN UPDATE !
+						
+						//Download the main file
+						$.ajax(
+							{
+								url: "https://rawgit.com/pluginId/release/src/main.js",
+								success: function(data)
+								{
+									// Save the data locally
+									localStorage.setItem("SkyX/Plugins/" + pluginId + "/main.js", data);
+
+									// Download all of the local dependencies
+									var pluginObject = eval(data)();
+									for (let file in pluginObject.localDependencies)
+									{
+										$.ajax(
+											{
+												url : "https://rawgit.com/pluginId/release/src/" + file,
+												success : function(data)
+												{
+													localStorage.setItem("SkyX/Plugins/" + pluginId + "/" + file, data);
+												},
+												error: function()
+												{
+													notify("An error occured while updating " + pluginId + ", please contant the plugin developer");
+												}
+											}
+										);
+									}
+
+									// Pretty much done with this plugin, going on to other ones
+									// Update the plugins table with the new commit hash
+									var pluginsTable = localStorage.getItem("SkyX/Plugins/PluginsTable");
+									pluginsTable[pluginId] = remoteCommitHash;
+
+									// Download all of the remote dependencies
+									for(var dependency in pluginObject.remoteDependencies)
+									{
+										updatePlugin(dependency);
+									}
+								},
+								error: function()
+								{
+									notify("An error occured while updating " + pluginId + ", please contant the plugin developer");
+								}
+							}
+						);
+					}
+				},
+				error: function()
+				{
+					notify("An error occured while updating " + pluginId + ", please contant the plugin developer");
+				}
+			}
+		);
+	};
+
+	for (let pluginId in localStorage.getItem("SkyX/Plugins/PluginsTable"))
+	{
+		UpdatePlugin(pluginId);
+	}
 }
 
 // TODO : Handle download failure better, see github issue
 // updates core.user.js and saves it into localStorage
-function updateSelf() {
+function updateSelf() 
+{
 	console.log("Enter updateSelf()");
 
 	// updating files
@@ -91,7 +302,6 @@ function updateSelf() {
 		url: "https://api.github.com/repos/geofs-plugins/plugin-manager-V2/commits/" + remoteBranch,
 
 		success: function(data) {
-			debug("Succesfuly got latest commit hash");
 			let latestRemoteCommitHash = data["sha"];
 
 			if (latestRemoteCommitHash != localStorage.getItem("SkyX/version")) {
@@ -109,7 +319,7 @@ function updateSelf() {
 				function waitForUpdate() {
 					if (filesFinished == filesToUpdate.length) {
 						if (hasFailed) {
-							notify("One of the files failed to download");
+							notify("One of the core files failed to download");
 
 							// deleting all of the files
 							for (var file in filesToUpdate) {
@@ -133,7 +343,6 @@ function updateSelf() {
 					$.ajax({
 						url: remoteContentUrl + filesToUpdate[file],
 						success: function(data) {
-							debug("got " + filesToUpdate[file]);
 							// if the ui hasn't been loaded yet, and the current file is the ui file
 							// the load the content you just fetched.
 							if ((!("SkyX/Core/ui.user.html" in localStorage)) && filesToUpdate[file] == "ui.user.html") {
@@ -145,18 +354,18 @@ function updateSelf() {
 						},
 
 						error: function() {
-							debug("failed to download" + filesToUpdate[file]);
+							notify("failed to update " + filesToUpdate[file]);
 							hasFailed = true;
 							filesFinished++;
 						}
 					});
 				}
 			} else {
-				debug("Everything is up to date");
+				debug("SkyX V2 is up to date");
 			}
 		},
 		error: function() {
-			debug("Getting latest commit hash failed");
+			notify("Getting latest commit hash failed");
 		}
 	});
 }
@@ -168,9 +377,9 @@ function main() {
 	if (typeof(Storage) !== "undefined") {
 		loadUi();
 
-		loadPlugins();
+		loadAvailablePlugins();
 
-		updatePlugin();
+		updatePlugins();
 
 		updateSelf();
 	} else {
